de580348bfc2ea68150aaa7167bd2d8a6f1d8b78
bt284 <tangbozeng@users.noreply.github.com>
2017-11-09 14:55:01 +0000

    Update denovo_assembly_megahit.sh

--- denovo_assembly_megahit.sh
+++ denovo_assembly_megahit.sh
@@ -2,3 +2,9 @@
 git clone https://github.com/voutcn/megahit.git
 cd megahit
 make -j 6
+mkdir ~/work
+cd ~/work
+curl -O -L https://s3.amazonaws.com/public.ged.msu.edu/ecoli_ref-5m.fastq.gz
+~/megahit/megahit --12 ecoli_ref-5m.fastq.gz -o ecoli
+cp ecoli/final.contigs.fa ecoli-assembly.fa #copy and save the denovo file first
+head ecoli-assembly.fa
